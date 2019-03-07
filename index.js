5cbc64e4c3006eef466b35cc233af5d2ce173520
Tami Churns <tchurns@paperlesspost.com>
2019-03-07 01:27:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const currentUser = 'Grace Hopper'
 const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!'
 const excitedWelcomeMessage = welcomeMessage.toUpperCase()
-const shortGreeting = 
+const shortGreeting = 'Welcome, E!'
